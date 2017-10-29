13718f5ee279fed0502ef5bacac283acd00522a8
Kazuhiro Komiya <kazuhiro@altar.jp>
2017-10-29 18:15:05 +0900

    add - .tigrc

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
+
+# bash
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.fzfcmd.sh ~/.fzfcmd.sh
+
+# editor
 ln -s ~/dotfiles/.emacs ~/.emacs
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
@@ -9,6 +13,7 @@ ln -s ~/dotfiles/.vim ~/.vim
 # git
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/.tigrc ~/.tigrc
 
 # tmux
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf

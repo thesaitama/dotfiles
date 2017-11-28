e5795545dd58e0e298a2031b8a97d3502337cf5e
Kazuhiro Komiya <kazuhiro@altar.jp>
2017-11-28 23:21:58 +0900

    add - quick lock

--- osx.el
+++ osx.el
@@ -21,6 +21,36 @@
 (setq interprogram-cut-function 'paste-to-osx)
 (setq interprogram-paste-function 'copy-from-osx)
 
+(defun open-mac (path)
+  (start-process "dired-open-mac" nil "open" path))
+
+;; ------------------------------------------------------------------------
+;; mac quick-lock
+
+(defun quicklook-file (path)
+  (interactive)
+  (defvar cur nil)
+  (defvar old nil)
+  (setq old cur)
+  (setq cur (start-process "ql-file" nil "qlmanage" "-p" path))
+  (when old (delete-process old)))
+
+(defun my-dired-open ()
+  (interactive)
+  (let ((exts-ql   '("jpeg" "jpg" "png" "gif" "tiff"))
+        (exts-open '("avi" "mkv" "mp4" "mts" "psd" "ai" "pdf")))
+     (cond ((file-accessible-directory-p (dired-get-file-for-visit))
+            (call-interactively 'dired-find-alternate-file))
+           ((member (downcase (file-name-extension (dired-get-file-for-visit))) exts-ql)
+            (funcall 'quicklook-file (dired-get-file-for-visit)))
+           ((member (downcase (file-name-extension (dired-get-file-for-visit))) exts-open)
+            (funcall 'open-mac (dired-get-file-for-visit)))
+           (t
+            (call-interactively 'dired-find-file-other-window)))))
+(add-hook 'dired-mode-hook
+          '(lambda ()
+             (define-key dired-mode-map "o" 'my-dired-open)))
+
 ;; ------------------------------------------------------------------------
 ;; osx-dictionary
 

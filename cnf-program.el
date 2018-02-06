16852daddab733c1055df214153ad234235e34d0
Kazuhiro Komiya <kazuhiro@altar.jp>
2018-02-06 23:15:31 +0900

    fix js2-mode auto-mode-alist

--- cnf-program.el
+++ cnf-program.el
@@ -209,8 +209,8 @@
 ;; js2-mode
 
 (require 'js2-mode)
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx$" . js2-jsx-mode))
 (add-hook 'js2-mode-hook #'js2-refactor-mode)
 
 ;; ------------------------------------------------------------------------

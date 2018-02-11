1654b0bf2f5ce87a2518330ddf68c87ec564978f
Kazuhiro Komiya <kazuhiro@altar.jp>
2018-02-11 10:08:17 +0900

    update helm settings

--- cnf-helm.el
+++ cnf-helm.el
@@ -9,8 +9,10 @@
 ;; ------------------------------------------------------------------------
 ;; helm
 
+(require 'helm)
 (require 'helm-config)
 (helm-mode +1)
+
 (define-key global-map (kbd "M-x") 'helm-M-x)
 (define-key global-map (kbd "C-c h") 'helm-mini)
 (define-key global-map (kbd "C-c i") 'helm-imenu)
@@ -18,12 +20,14 @@
 (define-key global-map (kbd "C-x C-r") 'helm-recentf)
 (define-key global-map (kbd "M-y") 'helm-show-kill-ring)
 (define-key global-map (kbd "C-x b") 'helm-buffers-list)
+
 (define-key helm-map (kbd "TAB") 'helm-execute-persistent-action)
 (define-key helm-map (kbd "C-h") 'delete-backward-char)
 (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
 (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
 (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
 (define-key isearch-mode-map (kbd "C-o") 'helm-occur-from-isearch)
+
 ;; Emulate `kill-line' in helm minibuffer
 (setq helm-delete-minibuffer-contents-from-point t)
 (defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
@@ -40,7 +44,18 @@
                   (unless (string-match "\\(?:/\\|\\`\\)\\.\\{2\\}\\'" file)
                     (funcall fcn file))))
 
+;; fuzzy match
+(setq helm-M-x-fuzzy-match t)
+(setq helm-locate-fuzzy-match t)
+(setq helm-buffers-fuzzy-matching t)
+(setq helm-recentf-fuzzy-match t)
+(setq helm-semantic-fuzzy-match t)
+(setq helm-imenu-fuzzy-match t)
+(setq helm-apropos-fuzzy-match t)
+(setq helm-lisp-fuzzy-completion t)
+
 (setq helm-split-window-inside-p t)
+
 ;; auto resize
 (setq helm-autoresize-max-height 0)
 (setq helm-autoresize-min-height 40)

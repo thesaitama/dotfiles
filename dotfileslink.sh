bb51f09fde43cb24aa41f9dfe8134e5df18e298c
Kazuhiro Komiya <kazuhiro@altar.jp>
2018-02-26 23:01:10 +0900

    fix link

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -8,14 +8,19 @@ ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.inputrc ~/.inputrc
 ln -s ~/dotfiles/.fzfcmd.sh ~/.fzfcmd.sh
 
-# editor
+# emacs
 ln -s ~/dotfiles/.emacs.el ~/.emacs.el
+
+# vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
 
 # neovim
-ln -s ~/dotfiles/.vim ~/.config/nvim
-ln -s ~/dotfiles/.vimrc ~/.config/nvim/init.vim
+# ~/.config/nvim@ -> ~/dotfiles/.vim
+# ~/.config/nvim/.vim@ -> ~/.vim
+# ~/.config/nvim/init.vim@ -> ~/.vimrc
+ln -s ~/.vim ~/.config/nvim
+ln -s ~/.vimrc ~/.config/nvim/init.vim
 
 # git
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig

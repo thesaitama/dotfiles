a6e28d8e6e20c90def9757f3dd3caa83b13d7a9c
Kazuhiro Komiya <kazuhiro@altar.jp>
2018-02-26 23:36:58 +0900

    improve isearch

--- cnf-basics.el
+++ cnf-basics.el
@@ -147,13 +147,30 @@
 ;; which fucntion
 (which-function-mode 1)
 
+;; ignore case
+(setq read-buffer-completion-ignore-case t)
+(setq read-file-name-completion-ignore-case t)
+
+;; ------------------------------------------------------------------------
+;; isearch
+
 ;; search
 (setq case-fold-search t)
 (setq isearch-case-fold-search t)
 
-;; ignore case
-(setq read-buffer-completion-ignore-case t)
-(setq read-file-name-completion-ignore-case t)
+(defadvice isearch-mode
+    (around isearch-mode-default-string
+            (forward &optional regexp op-fun recursive-edit word-p) activate)
+  (if (and transient-mark-mode mark-active (not (eq (mark) (point))))
+      (progn
+        (isearch-update-ring (buffer-substring-no-properties (mark) (point)))
+        (deactivate-mark)
+        ad-do-it
+        (if (not forward)
+            (isearch-repeat-backward)
+          (goto-char (mark))
+          (isearch-repeat-forward)))
+    ad-do-it))
 
 ;; ------------------------------------------------------------------------
 ;; uniquify

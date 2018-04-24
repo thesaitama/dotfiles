66f7d169507c7484b7bc1affa204970ebb012b82
Kazuhiro Komiya <kazuhiro@altar.jp>
2018-04-24 21:34:18 +0900

    improve python support

--- elisp/cnf-program.el
+++ elisp/cnf-program.el
@@ -355,9 +355,9 @@
 ;; ------------------------------------------------------------------------
 ;; python-mode
 
-(setq auto-mode-alist
-      (cons '("\\.py$" . python-mode) auto-mode-alist))
 (autoload 'python-mode "python-mode" "Python editing mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
 
 ;; for Ipython
 (setq python-shell-interpreter-args "--simple-prompt --pprint")

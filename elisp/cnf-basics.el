1af75237b9c60874427b213487bda2da810fcda2
Kazuhiro Komiya <kazuhiro@altar.jp>
2018-12-27 19:06:42 +0900

    introduce emacs integration on bash

--- elisp/cnf-basics.el
+++ elisp/cnf-basics.el
@@ -400,6 +400,27 @@
 (add-hook 'dired-load-hook (lambda () (load "dired-x")))
 
 ;; ------------------------------------------------------------------------
+;; return-current-working-directory
+
+;; https://uwabami.github.io/cc-env/Emacs.html
+
+(defun return-current-working-directory-to-shell ()
+  (expand-file-name
+   (with-current-buffer
+       (if (featurep 'elscreen)
+           (let* ((frame-confs (elscreen-get-frame-confs (selected-frame)))
+                  (num (nth 1 (assoc 'screen-history frame-confs)))
+                  (cur-window-conf
+                   (assoc 'window-configuration
+                          (assoc num (assoc 'screen-property frame-confs))))
+                  (marker (nth 2 cur-window-conf)))
+             (marker-buffer marker))
+         (nth 1
+              (assoc 'buffer-list
+                     (nth 1 (nth 1 (current-frame-configuration))))))
+     default-directory)))
+
+;; ------------------------------------------------------------------------
 ;; executable make buffer
 
 (add-hook 'after-save-hook

41012df8abc946b8a1616207cf5c58d4c7337527
Kazuhiro Komiya <kazuhiro@altar.jp>
2018-12-27 11:33:16 +0900

    make compatibility for before 24.5

--- elisp/cnf-calendar.el
+++ elisp/cnf-calendar.el
@@ -9,22 +9,24 @@
 ;; ------------------------------------------------------------------------
 ;; calendar
 
-(with-eval-after-load "calendar"
-  (add-hook 'calendar-today-visible-hook 'calendar-mark-today)
+(eval-after-load 'calendar
+  '(progn
+     (add-hook 'calendar-today-visible-hook 'calendar-mark-today)
 
-  (when (require 'japanese-holidays nil t)
-    (setq calendar-holidays
-          (append japanese-holidays
-                  holiday-local-holidays holiday-other-holidays))
-    (setq calendar-mark-holidays-flag t)
-    (setq mark-holidays-in-calendar t)
-    (setq japanese-holiday-weekend-marker
-          '(holiday nil nil nil nil nil japanese-holiday-saturday))
-    (setq japanese-holiday-weekend '(0 6))
-    (add-hook 'calendar-today-visible-hook 'japanese-holiday-mark-weekend)
-    (add-hook 'calendar-today-visible-hook 'calendar-mark-today)
-    (add-hook 'calendar-today-invisible-hook 'japanese-holiday-mark-weekend)
-  )
+     (when (require 'japanese-holidays nil t)
+       (setq calendar-holidays
+             (append japanese-holidays
+                     holiday-local-holidays holiday-other-holidays))
+       (setq calendar-mark-holidays-flag t)
+       (setq mark-holidays-in-calendar t)
+       (setq japanese-holiday-weekend-marker
+             '(holiday nil nil nil nil nil japanese-holiday-saturday))
+       (setq japanese-holiday-weekend '(0 6))
+       (add-hook 'calendar-today-visible-hook 'japanese-holiday-mark-weekend)
+       (add-hook 'calendar-today-visible-hook 'calendar-mark-today)
+       (add-hook 'calendar-today-invisible-hook 'japanese-holiday-mark-weekend)
+       )
+     )
   )
 
 ;; ------------------------------------------------------------------------

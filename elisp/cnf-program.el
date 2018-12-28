e38da9bb1e6f158c0b3b41fd22c597fb06bdd8cc
Kazuhiro Komiya <kazuhiro@altar.jp>
2018-12-28 13:26:24 +0900

    introduce scheme settings

--- elisp/cnf-program.el
+++ elisp/cnf-program.el
@@ -513,6 +513,30 @@
                                (flycheck-mode)))
 
 ;; ------------------------------------------------------------------------
+;; scheme
+
+(setq process-coding-system-alist
+      (cons '("gosh" utf-8 . utf-8) process-coding-system-alist))
+(setq scheme-program-name "/opt/local/bin/gosh -i")
+
+(autoload 'scheme-mode "cmuscheme" "Major mode for Scheme." t)
+(autoload 'run-scheme "cmuscheme" "Run an inferior Scheme process." t)
+
+(defun scheme-other-window ()
+  "Run Gauche on other window."
+  (interactive)
+  (split-window-below (/ (frame-width) 2))
+  (let ((buf-name (buffer-name (current-buffer))))
+    (scheme-mode)
+    (switch-to-buffer-other-window
+     (get-buffer-create "*scheme*"))
+    (run-scheme scheme-program-name)
+    (switch-to-buffer-other-window
+     (get-buffer-create buf-name))))
+
+;; > sudo port install gauche
+
+;; ------------------------------------------------------------------------
 ;; clojure-mode
 
 (add-hook 'cider-mode-hook #'clj-refactor-mode)

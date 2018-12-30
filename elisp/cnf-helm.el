7e00c79c1db1fbbcf913bd0b49be8d8f1b2edcb0
Kazuhiro Komiya <kazuhiro@altar.jp>
2018-12-30 16:20:37 +0900

    change helm-imenu to helm-semantic-or-imenu

--- elisp/cnf-helm.el
+++ elisp/cnf-helm.el
@@ -15,7 +15,8 @@
 
 (define-key global-map (kbd "M-x") 'helm-M-x)
 (define-key global-map (kbd "C-c h") 'helm-mini)
-(define-key global-map (kbd "C-c i") 'helm-imenu)
+;; (define-key global-map (kbd "C-c i") 'helm-imenu)
+(define-key global-map (kbd "C-c i") 'helm-semantic-or-imenu)
 (define-key global-map (kbd "C-x C-f") 'helm-find-files)
 (define-key global-map (kbd "C-x C-r") 'helm-recentf)
 (define-key global-map (kbd "M-y") 'helm-show-kill-ring)
